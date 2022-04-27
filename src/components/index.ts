d89d40708ac8f80dc2f134e31ee9f8cec16a0368
Rafael <perchespierri@gmail.com>
2022-04-27 08:22:32 -0300

    feat: Components barrel file

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import Card from "./Card";
+import List from "./List";
+import Pokemon from "./Pokemon";
+import Header from "./Header";
+import PageNotFound from "./PageNotFound";
+import PageButton from "./PageButton";
+
+export { Card, List, Pokemon, Header, PageNotFound, PageButton }
